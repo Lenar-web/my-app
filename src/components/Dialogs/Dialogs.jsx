d4e7c4810212d0bbbd74bced5faeca03ad634272
Ленар <lenar.evsrafyev@mail.ru>
2019-08-16 23:32:51 +0300

    поправил расположение данных

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -2,30 +2,36 @@ import React from 'react';
 import DialogsItem from './DialogsItem/DialogsItem';
 import Message from './Message/Message';
 
-let dialogsData = [
-  {name: "Линда Лохан", message: "Привет! Ты как?", id: 1, time: "Минуту назад"},
-  {name: "Юлия Кокс", message: "Я вижу", id: 2, time: "Час назад"},
-  {name: "София Ли", message: "Привет! Ты как?", id: 3, time: "13 часов назад"},
-  {name: "Джон Дои", message: "Привет! что нового", id: 4, time: "Вчера"},
-  {name: "Анна Енг", message: "Ты тут?", id: 5, time: "2 дня назад"},
-  {name: "Ричард Билл", message: "Я поеду сегодня", id: 6, time: "2 дня назад"}
-];
-
-let messagesData = [
-  {name: "Линда Лохан", message: "Привет! Ты как?", id: 1, time: "3 дня назад", position: 'left'},
-  {name: "Ленар Евстафьев", message: "Отлично", id: 2, time: "3 дня назад", position: 'right'},
-  {name: "Ленар Евстафьев", message: "Отлично", id: 3, time: "3 дня назад", position: 'right'},
-  {name: "Ленар Евстафьев", message: "Отлично", id: 4, time: "3 дня назад", position: 'right'},
-  {name: "Линда Лохан", message: "Что делаешь?", id: 5, time: "30 минут назад", position: 'left'},
-  {name: "Ленар Евстафьев", message: "Работаю", id: 6, time: "Минуту назад", position: 'right'}
-];
 
 
-let dialog = dialogsData.map(d => <DialogsItem name={d.name} message={d.message} id={d.id} time={d.time}/>);
-let message = messagesData.map(m => <Message name={m.name} message={m.message} id={m.id} time={m.time} position={m.position} />);
 
 
-let Dialogs = (props) => {
+
+
+const Dialogs = (props) => {
+  
+  let dialogsData = [
+    {name: "Линда Лохан", message: "Привет! Ты как?", id: 1, time: "Минуту назад"},
+    {name: "Юлия Кокс", message: "Я вижу", id: 2, time: "Час назад"},
+    {name: "София Ли", message: "Привет! Ты как?", id: 3, time: "13 часов назад"},
+    {name: "Джон Дои", message: "Привет! что нового", id: 4, time: "Вчера"},
+    {name: "Анна Енг", message: "Ты тут?", id: 5, time: "2 дня назад"},
+    {name: "Ричард Билл", message: "Я поеду сегодня", id: 6, time: "2 дня назад"}
+  ];
+  
+  let messagesData = [
+    {name: "Линда Лохан", message: "Привет! Ты как?", id: 1, time: "3 дня назад", position: 'left'},
+    {name: "Ленар Евстафьев", message: "Отлично", id: 2, time: "3 дня назад", position: 'right'},
+    {name: "Ленар Евстафьев", message: "Отлично", id: 3, time: "3 дня назад", position: 'right'},
+    {name: "Ленар Евстафьев", message: "Отлично", id: 4, time: "3 дня назад", position: 'right'},
+    {name: "Линда Лохан", message: "Что делаешь?", id: 5, time: "30 минут назад", position: 'left'},
+    {name: "Ленар Евстафьев", message: "Работаю", id: 6, time: "Минуту назад", position: 'right'}
+  ];
+
+  let dialog = dialogsData.map(d => <DialogsItem name={d.name} message={d.message} id={d.id} time={d.time}/>);
+  let message = messagesData.map(m => <Message name={m.name} message={m.message} id={m.id} time={m.time} position={m.position} />);
+
+
 return (
   <div className="chat-room">
     <div className="row">
